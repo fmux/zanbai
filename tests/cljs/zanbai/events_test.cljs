de1108dfbffcfa42920b6c7b0937b8e044055a76
Fabian Müller <fabian@zentralblatt-math.org>
2016-12-04 15:57:13 +0100

    update event handler tests

--- tests/cljs/zanbai/events_test.cljs
+++ tests/cljs/zanbai/events_test.cljs
@@ -1,25 +1,30 @@
 (ns ^:figwheel-load zanbai.events-test
   (:require [cljs.test :refer-macros [deftest is testing]]
-            [zanbai.events :refer [got-pending-messages]]))
+            [zanbai.events :refer [got-pending-messages]]
+            [zanbai.db :as db]))
 
 (deftest test-got-pending-messages
   (testing "Event :got-pending-messages"
-    (let [db-initial {:username "Fabi"
-                      :users ["Adam" "Fabi"]
-                      :conversations {}}
+    (let [db-initial {::db/username "Fabi"
+                      ::db/users ["Adam" "Fabi"]
+                      ::db/conversations {}}
           first-result {:users ["Adam" "Fabi"]
-                        :pending-messages {"f193e160-b86f-11e6-a98b-4ede1694ae03" [{:from "Adam" :text "Hello!"}]}}
-          first-db {:username "Fabi"
-                    :users ["Adam" "Fabi"]
-                    :conversations {"f193e160-b86f-11e6-a98b-4ede1694ae03" [{:from "Adam" :text "Hello!"}]}}
+                        :pending-messages {"f193e160-b86f-11e6-a98b-4ede1694ae03"
+                                           [{:from "Adam" :text "Hello!"}]}}
+          first-db {::db/username "Fabi"
+                    ::db/users ["Adam" "Fabi"]
+                    ::db/conversations {"f193e160-b86f-11e6-a98b-4ede1694ae03"
+                                        [{:from "Adam" :text "Hello!"}]}}
           first-expected {:db first-db
                           :dispatch-later [{:ms 1000, :dispatch [:get-pending-messages]}]}
           second-result {:users ["Adam" "Fabi"]
-                         :pending-messages {"f193e160-b86f-11e6-a98b-4ede1694ae03" [{:from "Adam" :text "Anybody there?"}]}}
-          second-db {:username "Fabi"
-                     :users ["Adam" "Fabi"]
-                     :conversations {"f193e160-b86f-11e6-a98b-4ede1694ae03" [{:from "Adam" :text "Hello!"}
-                                                                             {:from "Adam" :text "Anybody there?"}]}}
+                         :pending-messages {"f193e160-b86f-11e6-a98b-4ede1694ae03"
+                                            [{:from "Adam" :text "Anybody there?"}]}}
+          second-db {::db/username "Fabi"
+                     ::db/users ["Adam" "Fabi"]
+                     ::db/conversations {"f193e160-b86f-11e6-a98b-4ede1694ae03"
+                                         [{:from "Adam" :text "Hello!"}
+                                          {:from "Adam" :text "Anybody there?"}]}}
           second-expected {:db second-db
                            :dispatch-later [{:ms 1000, :dispatch [:get-pending-messages]}]}]
       (testing "Starting new conversation"
